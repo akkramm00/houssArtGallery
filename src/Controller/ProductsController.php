08225e9457b49ee6b8d91f864dd5c2ece0c67979
akkramm00 <127404065+akkramm00@users.noreply.github.com>
2024-02-03 15:40:25 +0100

    add products controller

---
+++ src/Controller/ProductsController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\ProductsRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Knp\Component\Pager\PaginatorInterface;
+
+class ProductsController extends AbstractController
+{
+    #[Route('/products', name: 'products.index', methods: ['GET'])]
+    public function index(
+        ProductsRepository $repository,
+        PaginatorInterface $paginator,
+        Request $request
+    ): Response {
+        $products = $paginator->paginate(
+            $repository->findAll(),
+            $request->query->getInt('page', 1),
+            10
+        );
+
+        return $this->render('pages/products/index.html.twig', [
+            'products' => $products
+        ]);
+    }
+}
