a4945d5629c26dabca8e4ae185ab9ebc48ae5c32
akkramm00 <127404065+akkramm00@users.noreply.github.com>
2024-02-06 23:18:53 +0100

    add viewCollection

--- src/Controller/CollectionController.php
+++ src/Controller/CollectionController.php
@@ -2,17 +2,36 @@
 
 namespace App\Controller;
 
+use App\Repository\ColletionRepository;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class CollectionController extends AbstractController
 {
+    /**
+     * THis controller display All collections
+     *
+     * @param ColletionRepository $repository
+     * @param PaginatorInterface $paginator
+     * @param Request $request
+     * @return Response
+     */
     #[Route('/collection', name: 'collection.index', methods: ['GET'])]
-    public function index(): Response
-    {
+    public function index(
+        ColletionRepository $repository,
+        PaginatorInterface $paginator,
+        Request $request
+    ): Response {
+        $colletion = $paginator->paginate(
+            $repository->findAll(),
+            $request->query->getInt('page', 1),
+            10
+        );
         return $this->render('pages/collection/index.html.twig', [
-            'controller_name' => 'CollectionControlllerController',
+            'colletion' => $colletion,
         ]);
     }
 }
