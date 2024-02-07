e90499177776e00ed8630b09f8858ba910b2800f
akkramm00 <127404065+akkramm00@users.noreply.github.com>
2024-02-07 16:25:16 +0100

    add authentificateForm branch for user authetification

---
+++ src/Controller/SecurityController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class SecurityController extends AbstractController
+{
+    /**
+     * This Controller allow us to loginUp
+     *
+     * @param AuthenticationUtils $authenticationUtils
+     * @return Response
+     */
+    #[Route('/connexion', name: 'security.login', methods: ['GET', 'POST'])]
+    public function login(AuthenticationUtils $authenticationUtils): Response
+    {
+        $error = $authenticationUtils->getLastAuthenticationError();
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('pages/security/login.html.twig', [
+            'last_username' => $lastUsername,
+            'error'         => $error,
+        ]);
+    }
+
+    /********************************************************************************************* */
+
+    #[Route('/deconnexion', name: 'security.logout', methods: ['GET', 'POST'])]
+    public function logout()
+    {
+        // Nothing to do here !
+    }
+}
