5bb812cd6ffe2570ee0cdc734d158fac00b3a13e
akkramm00 <127404065+akkramm00@users.noreply.github.com>
2024-02-07 01:59:35 +0100

    add collectionCRUD branch for create the CRUD of collection

--- src/Controller/CollectionController.php
+++ src/Controller/CollectionController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\Colletion;
+use App\Form\CollectionType;
 use App\Repository\ColletionRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -33,5 +36,106 @@ class CollectionController extends AbstractController
         return $this->render('pages/collection/index.html.twig', [
             'colletion' => $colletion,
         ]);
+    }
+    /************************************************************************ */
+    /**
+     * This controller allow us to create a new collection
+     *
+     * @param Request $request
+     * @param EntityManagerInterface $manager
+     * @return Response
+     */
+    #[Route('/collection/nouveau', 'collection.new', methods: ['GET', 'POST'])]
+    public function new(
+        Request $request,
+        EntityManagerInterface $manager,
+    ): Response {
+        $colletion = new Colletion();
+        $form = $this->createForm(CollectionType::class, $colletion);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $colletion = $form->getData();
+
+            $manager->persist($colletion);
+            $manager->flush();
+
+            $this->addFlash(
+                'success',
+                'Votre collection a été créé avec succès !'
+            );
+
+            return $this->redirectToRoute('collection.index');
+        }
+
+        return $this->render('pages/collection/new.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+    /*********************************************************** */
+    /**
+     * This controller allow us to edit a collection
+     *
+     * @param ColletionRepository $repository
+     * @param Request $request
+     * @param EntityManagerInterface $manager
+     * @param [type] $id
+     * @return Response
+     */
+    #[Route('/collection/edition/{id}', 'collection.edit', methods: ['GET', 'POST'])]
+    public function edit(
+        ColletionRepository $repository,
+        Request $request,
+        EntityManagerInterface $manager,
+        $id
+    ): Response {
+        $colletion = $repository->findOneBy(["id" => $id]);
+        $form = $this->createForm(CollectionType::class, $colletion);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $colletion = $form->getData();
+
+            $manager->persist($colletion);
+            $manager->flush();
+
+            $this->addFlash(
+                'success',
+                'Votre collection a été modifié avec succès !'
+            );
+
+            return $this->redirectToRoute('collection.index');
+        }
+        return $this->render('pages/collection/edit.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+    /************************************************************************ */
+    #[Route('/collection/suppression/{id}', 'collection.delete', methods: ['GET', 'POST'])]
+    public function delete(
+        EntityManagerInterface $manager,
+        ColletionRepository $repository,
+        Colletion $colletion,
+        $id
+    ): Response {
+        $colletion = $repository->findOneBy(["id" => $id]);
+        if (!$colletion) {
+            $this->addflash(
+                'warning',
+                'La collection en question n\'a pas été trouvée !'
+            );
+
+            return $this->redirectToRoute('collection.index');
+        }
+        $manager->remove($colletion);
+        $manager->flush();
+
+        $this->addflash(
+            'success',
+            'La collection a été supprimée avec succès !'
+        );
+
+        return $this->redirectToRoute('collection.index');
     }
 }
--- src/Form/CollectionType.php
+++ src/Form/CollectionType.php
@@ -2,7 +2,10 @@
 
 namespace App\Form;
 
+use App\Entity\Colletion;
 use App\Entity\Products;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -10,10 +13,9 @@ use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\MoneyType;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Validator\Constraints as Assert;
 
-class ProductsType extends AbstractType
+class CollectionType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
@@ -30,49 +32,6 @@ class ProductsType extends AbstractType
                 ],
                 'constraints' => [
                     new Assert\Length(['min' => 2, 'max' => 50]),
-                    new Assert\NotBlank()
-                ]
-            ])
-            ->add('price', MoneyType::class, [
-                'attr' => [
-                    'class' => 'form-control',
-                ],
-                'label' => 'Prix',
-                'label_attr' => [
-                    'class' => 'form-label mt-2'
-                ],
-                'constraints' => [
-                    new Assert\Positive(),
-                    new Assert\NotBlank()
-                ]
-            ])
-            ->add('size', textType::class,  [
-                'attr' => [
-                    'class' => 'form-control',
-                    'minlength' => '2',
-                    'maxlength' => '50'
-                ],
-                'label' => 'Taille',
-                'label_attr' => [
-                    'class' => 'form-label mt-2'
-                ],
-                'constraints' => [
-                    new Assert\Length(['min' => 2, 'max' => 50]),
-                    new Assert\NotBlank()
-                ]
-            ])
-            ->add('property', TextareaType::class, [
-                'attr' => [
-                    'class' => 'form-control',
-                    'minlength' => '2',
-                    'maxlength' => '2000'
-                ],
-                'label' => 'Description',
-                'label_attr' => [
-                    'class' => 'form-label mt-2'
-                ],
-                'constraints' => [
-                    new Assert\Length(['min' => 2, 'max' => 2000]),
                     new Assert\NotBlank()
                 ]
             ])
@@ -106,40 +65,65 @@ class ProductsType extends AbstractType
                     new Assert\NotBlank()
                 ]
             ])
-            // ->add('imageFile', FileType::class, [
-            //     'attr' => [
-            //         'class' => 'form-control'
-            //     ],
-            //     'label' => 'Image du produit',
-            //     'label_attr' => [
-            //         'class' => 'form-label mt-4'
-            //     ]
-            // ])
-            ->add('isPublic', CheckboxType::class, [
+            ->add('description', TextareaType::class, [
+                'attr' => [
+                    'class' => 'form-control',
+                    'minlength' => '2',
+                    'maxlength' => '2000'
+                ],
+                'label' => 'Description',
+                'label_attr' => [
+                    'class' => 'form-label mt-2'
+                ],
+                'constraints' => [
+                    new Assert\Length(['min' => 2, 'max' => 2000]),
+                    new Assert\NotBlank()
+                ]
+            ])
+            ->add('price', MoneyType::class, [
+                'attr' => [
+                    'class' => 'form-control',
+                ],
+                'label' => 'Prix',
+                'label_attr' => [
+                    'class' => 'form-label mt-2'
+                ],
+                'constraints' => [
+                    new Assert\Positive(),
+                    new Assert\NotBlank()
+                ]
+            ])
+            ->add('isFavorite', CheckboxType::class, [
                 'attr' => [
                     'class' => 'form-check-input',
                 ],
-                'label' => 'Public',
+                'label' => 'Favoris ?',
                 'label_attr' => [
                     'class' => 'form-check-label'
                 ],
                 'constraints' => [
                     new Assert\NotNull()
                 ],
-                'required' => true
+                'required' => false
+            ])
+            ->add('products', EntityType::class, [
+                'class' => Products::class,
+                'choice_label' => 'name',
+                'multiple' => true,
+                'expanded' => true,
             ])
             ->add('submit', submitType::class, [
                 'attr' => [
                     'class' => 'btn btn-warning mt-2'
                 ],
-                'label' => 'Créer un nouveau produit'
+                'label' => 'Créer une nouvelle collection'
             ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
-            'data_class' => Products::class,
+            'data_class' => Colletion::class,
         ]);
     }
 }
